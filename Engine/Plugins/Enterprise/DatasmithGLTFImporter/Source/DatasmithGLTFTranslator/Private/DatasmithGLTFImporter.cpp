1a7254994046fc9e268f04d404060477ca87a4c9
emil kirichev <Emil.Kirichev@xa.epicgames.com>
2021-10-13 08:33:45 -0400

    Datasmith gltf importer does not read framerate from the file
    #jira UE-120277
    #rb JeanLuc.Corenthin
    
    [CL 17798825 by emil kirichev in 5.0 branch]

--- Engine/Plugins/Enterprise/DatasmithGLTFImporter/Source/DatasmithGLTFTranslator/Private/DatasmithGLTFImporter.cpp
+++ Engine/Plugins/Enterprise/DatasmithGLTFImporter/Source/DatasmithGLTFTranslator/Private/DatasmithGLTFImporter.cpp
@@ -61,6 +61,7 @@ FDatasmithGLTFImporter::FDatasmithGLTFImporter(TSharedRef<IDatasmithScene>& OutS
 	{
 		bGenerateLightmapUVs = InOptions->bGenerateLightmapUVs;
 		ImportScale = InOptions->ImportScale;
+		bAnimationFrameRateFromFile = InOptions->bAnimationFrameRateFromFile;
 	}
 }
 
@@ -72,6 +73,7 @@ void FDatasmithGLTFImporter::SetImportOptions(UDatasmithGLTFImportOptions* InOpt
 	{
 		bGenerateLightmapUVs = InOptions->bGenerateLightmapUVs;
 		ImportScale = InOptions->ImportScale;
+		bAnimationFrameRateFromFile = InOptions->bAnimationFrameRateFromFile;
 	}
 }
 
@@ -341,7 +343,7 @@ bool FDatasmithGLTFImporter::SendSceneToDatasmith()
 	}
 
 	AnimationImporter->CurrentScene = &DatasmithScene.Get();
-	AnimationImporter->CreateAnimations(*GLTFAsset);
+	AnimationImporter->CreateAnimations(*GLTFAsset, bAnimationFrameRateFromFile);
 
 	return true;
 }
