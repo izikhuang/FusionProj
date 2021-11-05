17314cbbe8112df2d16ce5051f116b5d501e71ea
Zhenglin Geng <zhenglin.geng@epicgames.com>
2021-11-04 21:10:14 -0400

    Undo //UE5/Release-5.0/Engine/Source/Runtime/Experimental/Chaos/... changelist 18067512
    [CL 18067582 by Zhenglin Geng in 5.0 branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/SolverCollisionContainer.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/SolverCollisionContainer.cpp
@@ -114,7 +114,7 @@ namespace Chaos
 					WorldContactNormal);
 
 				// Calculate the target normal velocity based on restitution
-				FReal WorldContactVelocityTargetNormal = FReal(0); // If two particles are already separating, this can zero out their normal velocity and cause loss of momentum. 
+				FReal WorldContactVelocityTargetNormal = FReal(0);
 				if (Restitution > FReal(0))
 				{
 					const FVec3 ContactVelocity = Solver.GetManifoldPoint(ManifoldPointIndex).CalculateContactVelocity(Solver.SolverBody0(), Solver.SolverBody1());
@@ -122,10 +122,6 @@ namespace Chaos
 					if (ContactVelocityNormal < -RestitutionVelocityThreshold)
 					{
 						WorldContactVelocityTargetNormal = -Restitution * ContactVelocityNormal;
-					}
-					else if (ContactVelocityNormal > RestitutionVelocityThreshold)
-					{
-						WorldContactVelocityTargetNormal = ContactVelocityNormal; // @todo(zhenglin): Ideally we should not apply impulse in this case.
 					}
 				}
 
