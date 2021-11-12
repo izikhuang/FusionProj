9a3a7839a48b13810812637ebc059768a49ce5ca
Michael Noland <michael.noland@epicgames.com>
2021-11-12 16:28:59 -0500

    Added support for cm/s as a speed unit #jira UE-134861 #rb andrew.rodham
    [CL 18174854 by Michael Noland in 5.0 branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/NoExportTypes.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/NoExportTypes.h
@@ -318,7 +318,7 @@ enum class EUnit : uint8
 	Degrees, Radians,
 	
 	/** Speed units */
-	MetersPerSecond, KilometersPerHour, MilesPerHour,
+	CentimetersPerSecond, MetersPerSecond, KilometersPerHour, MilesPerHour,
 	
 	/** Temperature units */
 	Celsius, Farenheit, Kelvin,
