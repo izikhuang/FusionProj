b9627477850b1e5bf3e8fe078240402174d81a21
Matt Peters <Matt.Peters@epicgames.com>
2021-11-23 21:59:14 -0500

    Farm instrumentation: Add a new required cpp to test whether there is a problem syncing new files on the farm. #robomerge ignore #rb None, Trivial #rnx
    [CL 18279574 by Matt Peters in 5.0 branch]

---
+++ Engine/Source/Editor/UnrealEd/Private/Cooker/TestFunctionInNewCpp.cpp
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HAL/Platform.h"
+
+#ifndef __clang__
+#pragma message("Compiling TestFunctionInNewCpp.cpp")
+#endif
+
+void TestFunctionInNewCpp()
+{
+
+}
