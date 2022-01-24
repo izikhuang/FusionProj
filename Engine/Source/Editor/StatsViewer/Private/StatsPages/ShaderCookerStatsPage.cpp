1189aa23d98f56a1c5f68e7ed0159de5b55c3ddf
christopher waters <christopher.waters@epicgames.com>
2022-01-24 15:32:11 -0500

    Fixing shader cooker stats showing the wrong shader format names. PlatformNames is indexed by SP_ enum so we have to put each format at the correct index. Also uses the DDSPI to determine if a format is valid to display at all.
    #jira UE-139041
    #rb arciel.rekman, jason.nadro, lukas.hermanns, mihnea.balta
    #preflight 61ef08a84018b3d62e738a0c
    
    [CL 18713611 by christopher waters in 5.0 branch]

--- Engine/Source/Editor/StatsViewer/Private/StatsPages/ShaderCookerStatsPage.cpp
+++ Engine/Source/Editor/StatsViewer/Private/StatsPages/ShaderCookerStatsPage.cpp
@@ -118,42 +118,27 @@ public:
 void FShaderCookerStats::Initialize(uint32 Index)
 {
 	TArray<FString> PlatformNames;
-	for (int32 Platform = 0; Platform < SP_NumPlatforms; ++Platform)
-	{
-		// ShaderPlatformToShaderFormatName asserts if it's passed a deprecated value, so we'll filter out the removed platforms here.
-		PRAGMA_DISABLE_DEPRECATION_WARNINGS
-		switch (Platform)
-		{
-			case SP_OPENGL_SM4_REMOVED:
-			case SP_PS4_REMOVED:
-			case SP_OPENGL_PCES2_REMOVED:
-			case SP_XBOXONE_D3D12_REMOVED:
-			case SP_PCD3D_SM4_REMOVED:
-			case SP_OPENGL_SM5_REMOVED:
-			case SP_PCD3D_ES2_REMOVED:
-			case SP_OPENGL_ES2_ANDROID_REMOVED:
-			case SP_OPENGL_ES2_WEBGL_REMOVED:
-			case SP_OPENGL_ES2_IOS_REMOVED:
-			case SP_OPENGL_ES31_EXT_REMOVED:
-			case SP_METAL_SM5_NOTESS_REMOVED:
-			case SP_VULKAN_SM4_REMOVED:
-			case SP_METAL_MACES2_REMOVED:
-			case SP_SWITCH_REMOVED:
-			case SP_SWITCH_FORWARD_REMOVED:
-			case SP_VULKAN_SM5_LUMIN_REMOVED:
-			case SP_VULKAN_ES3_1_LUMIN_REMOVED:
-				continue;
-		}
-		PRAGMA_ENABLE_DEPRECATION_WARNINGS
+	PlatformNames.Reserve(SP_NumPlatforms);
 
-		FString FormatName = ShaderPlatformToShaderFormatName((EShaderPlatform)Platform).ToString();
-		if (FormatName.Len() > 0)
+	for (int32 PlatformIndex = 0; PlatformIndex < SP_NumPlatforms; ++PlatformIndex)
+	{
+		const EShaderPlatform Platform = static_cast<EShaderPlatform>(PlatformIndex);
+
+		const FName ShaderFormatName = FDataDrivenShaderPlatformInfo::IsValid(Platform)
+			? ShaderPlatformToShaderFormatName(Platform) : NAME_None;
+
+		if (ShaderFormatName != NAME_None)
 		{
+			FString FormatName = ShaderFormatName.ToString();
 			if (FormatName.StartsWith(TEXT("SF_")))
 			{
 				FormatName.MidInline(3, MAX_int32, false);
 			}
 			PlatformNames.Add(MoveTemp(FormatName));
+		}
+		else
+		{
+			PlatformNames.Add(TEXT("unknown"));
 		}
 	}
 	FShaderCookerStatsSet& Set = StatSets[Index];
