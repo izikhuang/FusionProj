7ea44f110740957aff79fb9d44890511c434c1c1
stu mckenna <Stu.McKenna@epicgames.com>
2022-01-28 14:31:46 -0500

    [Backout] - CL18769011 #fyi Florin.Pascu, Lukas.Hermanns Original CL Desc ----------------------------------------------------------------- Force DXC for NiagaraDataInterface shaders #jira UE-139770 #rb Lukas.Hermanns,Rob.Krajcarski,Stu.McKenna #preflight 61f255b1fc74f46b56567c9c
    #ushell-cherrypick of 18751452 by Florin.Pascu
    #preflight 61f3a2461f12674afbd65026
    
    [CL 18776594 by stu mckenna in 5.0 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x08190087, 0x4ED24371, 0xA2801F4C, 0xBD4A29D0);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x875FFA32, 0xD251374B, 0x961B9DF2, 0x5BA60E35);
