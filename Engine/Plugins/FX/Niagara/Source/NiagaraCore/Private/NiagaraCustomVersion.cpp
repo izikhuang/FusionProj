29a07ae437e91d2432c1bffbc3115939a9f570a6
Stu McKenna <Stu.McKenna@epicgames.com>
2022-02-04 12:49:07 -0500

    - Enable 3D dispatch on 3D grid / render target
    #rb rob.krajcarski
    #jira UE-141265
    #rnx
    #preflight 61fc7c475e1fd46228530673
    
    [CL 18865799 by Stu McKenna in 5.0 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -9,4 +9,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
 // Note: When encountering a conflict on this file please generate a new GUID
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xE3CDF783, 0x967B4505, 0x84B6F2B6, 0xC4DABAF6);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x28263C6C, 0x712E1E43, 0x9DF1105F, 0x272C6A8B);
