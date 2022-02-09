e417e303bb4d10cff4af016177f07c9c7e7aa8b8
nicholas goldstein <nicholas.goldstein@epicgames.com>
2022-02-09 16:57:20 -0500

    Fix bug with name aliasing datainterface sourced parametes in simulation stage scripts where dependency checking would fail.
    #jira UE-141615, FORT-448090
    #preflight 62042797a65a8a284628009c
    #rb Michael.Galetzka
    
    [CL 18925006 by nicholas goldstein in 5.0 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -9,4 +9,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
 // Note: When encountering a conflict on this file please generate a new GUID
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x66B7F651, 0xB1DE404D, 0xB078E35D, 0x8BC87228);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x3800E1E6, 0x0A9A41D8, 0x80A8EED4, 0x03F676EC);
