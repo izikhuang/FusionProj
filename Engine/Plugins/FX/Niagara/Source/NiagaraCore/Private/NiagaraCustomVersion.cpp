da0230211f90d0a81874fdb3c31596059108feae
Stu McKenna <Stu.McKenna@epicgames.com>
2022-02-09 13:59:04 -0500

    - Enable 3D dispatch on grid 3d to bypass driver issue and increase performance on aligned grids
    #rb devon.penney
    #jira UE-141701
    #rnx
    #preflight 6204081c0927956f66bbecd8
    
    [CL 18922244 by Stu McKenna in 5.0 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -9,4 +9,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
 // Note: When encountering a conflict on this file please generate a new GUID
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x28263C6C, 0x712E1E43, 0x9DF1105F, 0x272C6A8B);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x66B7F651, 0xB1DE404D, 0xB078E35D, 0x8BC87228);
