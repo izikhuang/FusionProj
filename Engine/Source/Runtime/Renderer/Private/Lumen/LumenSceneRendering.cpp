ef7a79dcbb02a59d319326610e62787e66be6c3b
Krzysztof Narkowicz <Krzysztof.Narkowicz@epicgames.com>
2022-02-19 12:39:11 -0500

    Lumen Landscape - fixed Landscape artifacts due to caching Landscape data without checking if data is ready and streamed in
    #preflight 620d046f01253d2e190e6aa8
    #jira UE-143307
    #rb Patrick.Kelly
    
    [CL 19063086 by Krzysztof Narkowicz in 5.0 branch]

--- Engine/Source/Runtime/Renderer/Private/Lumen/LumenSceneRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/Lumen/LumenSceneRendering.cpp
@@ -1249,7 +1249,10 @@ float ComputeMaxCardUpdateDistanceFromCamera(float LumenSceneViewDistance)
 	return MaxCardDistanceFromCamera + GLumenSceneCardCaptureMargin;
 }
 
-// Make sure all mesh rendering data is prepared before we render
+/**
+ * Make sure that all mesh rendering data is prepared before we render this primitive group
+ * @return Will return true it primitive group is ready to render or we need to wait until next frame
+ */
 bool UpdateStaticMeshes(FLumenPrimitiveGroup& PrimitiveGroup)
 {
 	bool bReadyToRender = true;
@@ -1270,14 +1273,21 @@ bool UpdateStaticMeshes(FLumenPrimitiveGroup& PrimitiveGroup)
 				PrimitiveSceneInfo->BeginDeferredUpdateStaticMeshesWithoutVisibilityCheck();
 				bReadyToRender = false;
 			}
+
+			if (PrimitiveGroup.bHeightfield && PrimitiveSceneInfo->Proxy->HeightfieldHasPendingStreaming())
+			{
+				bReadyToRender = false;
+			}
 		}
 	}
 
 	return bReadyToRender;
 }
 
-// Process a throttled number of Lumen surface cache add requests
-// It will make virtual and physical allocations, and evict old pages as required
+/**
+ * Process a throttled number of Lumen surface cache add requests
+ * It will make virtual and physical allocations, and evict old pages as required
+ */
 void ProcessLumenSurfaceCacheRequests(
 	const FViewInfo& MainView,
 	FVector LumenSceneCameraOrigin,
