9d398b88dd9a2d305b5e6fb71a16765a258d967c
Max Whitehead <Max.Whitehead@epicgames.com>
2022-03-03 16:27:46 -0500

    Store local inertia tensor/inverse tensor diagonals in vec3 instead of matrix, reducing memory. #jira FORT-444042 #rb cedric.caillaud #preflight 62212e4ea00412627d0e1a7b #lockdown michael.lentine
    [CL 19253465 by Max Whitehead in 5.0 branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/PhysicsProxy/SingleParticlePhysicsProxy.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/PhysicsProxy/SingleParticlePhysicsProxy.h
@@ -644,7 +644,7 @@ public:
 		});
 	}
 
-	const FMatrix33 I() const
+	const Chaos::TVec3<FRealSingle> I() const
 	{
 		return Read([](auto* Particle)
 		{
@@ -653,11 +653,11 @@ public:
 				return Rigid->I();
 			}
 
-			return FMatrix33(0, 0, 0);
+			return Chaos::TVec3<FRealSingle>(0, 0, 0);
 		});
 	}
 
-	void SetI(const FMatrix33& InI)
+	void SetI(const Chaos::TVec3<FRealSingle>& InI)
 	{
 		Write([&InI](auto* Particle)
 		{
@@ -668,7 +668,7 @@ public:
 		});
 	}
 
-	const FMatrix33 InvI() const
+	const Chaos::TVec3<FRealSingle> InvI() const
 	{
 		return Read([](auto* Particle)
 		{
@@ -677,11 +677,11 @@ public:
 				return Rigid->InvI();
 			}
 
-			return FMatrix33(0, 0, 0);
+			return Chaos::TVec3<FRealSingle>(0, 0, 0);
 		});
 	}
 
-	void SetInvI(const FMatrix33& InInvI)
+	void SetInvI(const Chaos::TVec3<FRealSingle>& InInvI)
 	{
 		Write([&InInvI](auto* Particle)
 		{
--- Engine/Source/Runtime/Experimental/ChaosCore/Public/Chaos/Core.h
+++ Engine/Source/Runtime/Experimental/ChaosCore/Public/Chaos/Core.h
@@ -42,6 +42,6 @@ namespace Chaos
 	// NOTE: if you get a merge conflict on the GUID, you must replace it with a new GUID - do not accept the source or target
 	// or you will likely get DDC version conflicts resulting in crashes during load.
 	// Core version string for Chaos data. Any DDC builder dependent on Chaos for serialization should depend on this version
-	static const FString ChaosVersionString = TEXT("773935A7-7F78-49BB-839A-2FC18D40599D");
+	static const FString ChaosVersionString = TEXT("6809AC86-276B-4AB2-B777-382005E80FAE");
 
 }
