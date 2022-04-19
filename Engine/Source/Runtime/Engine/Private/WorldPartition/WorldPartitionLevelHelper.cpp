495edeab35642db18e41c08dcdf02b05e67f22a8
Richard Malo <Richard.Malo@epicgames.com>
2022-04-19 15:34:54 -0400

    Fixed Level Instance Pivot ignored when running PIE/game. #jira UE-149624 #rb patrick.enfedaque #preflight 625f045c9c09ef439d9eb8ff #robomerge -UE5-Release-Engine-Staging
    [CL 19815089 by Richard Malo in 5.0 branch]

--- Engine/Source/Runtime/Engine/Private/WorldPartition/WorldPartitionLevelHelper.cpp
+++ Engine/Source/Runtime/Engine/Private/WorldPartition/WorldPartitionLevelHelper.cpp
@@ -13,6 +13,7 @@
 #include "Model.h"
 #include "UnrealEngine.h"
 #include "UObject/UObjectHash.h"
+#include "GameFramework/WorldSettings.h"
 #include "WorldPartition/ActorDescContainer.h"
 #include "WorldPartition/WorldPartitionActorDesc.h"
 #include "WorldPartition/WorldPartition.h"
@@ -387,10 +388,13 @@ bool FWorldPartitionLevelHelper::LoadActors(ULevel* InDestLevel, TArrayView<FWor
 					// Possible actor isn't found if Actor package failed to load because of missing dependencies (ex: deleted Blueprint)
 					if (DuplicatedActor)
 					{
+						AWorldSettings* WorldSettings = DuplicatedActor->GetLevel()->GetWorldSettings();
+						FTransform Transform = FTransform(WorldSettings->LevelInstancePivotOffset) * Mapping->ContainerTransform;
+
 						DuplicatedActor->Rename(*FString::Printf(TEXT("%s_%s"), *DuplicatedActor->GetName(), *Mapping->ContainerID.ToString()), InDestLevel, REN_NonTransactional | REN_ForceNoResetLoaders | REN_DoNotDirty | REN_DontCreateRedirectors);
 						USceneComponent* RootComponent = DuplicatedActor->GetRootComponent();
 
-						FLevelUtils::FApplyLevelTransformParams TransformParams(nullptr, Mapping->ContainerTransform);
+						FLevelUtils::FApplyLevelTransformParams TransformParams(nullptr, Transform);
 						TransformParams.Actor = DuplicatedActor;
 						TransformParams.bDoPostEditMove = false;
 						FLevelUtils::ApplyLevelTransform(TransformParams);
