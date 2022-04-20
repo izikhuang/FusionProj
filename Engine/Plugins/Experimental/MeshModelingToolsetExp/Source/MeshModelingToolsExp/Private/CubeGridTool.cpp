514858a72bc1ac7fe9c7e55aba3562d1559fba66
semion piskarev <semion.piskarev@epicgames.com>
2022-04-20 13:30:11 -0400

    MeshModelingTools: Fix crash in CubeGrid tool when undoing out of the tool after modifying the target.
    #rb Jimmy.Andrews
    #jira UE-148710
    #preflight 62602d8bdd47b4ad2173c525
    
    [CL 19833591 by semion piskarev in 5.0 branch]

--- Engine/Plugins/Experimental/MeshModelingToolsetExp/Source/MeshModelingToolsExp/Private/CubeGridTool.cpp
+++ Engine/Plugins/Experimental/MeshModelingToolsetExp/Source/MeshModelingToolsExp/Private/CubeGridTool.cpp
@@ -867,14 +867,17 @@ void UCubeGridTool::Shutdown(EToolShutdownType ShutdownType)
 	{
 		Cast<IPrimitiveComponentBackedTarget>(Target)->SetOwnerVisibility(true);
 
-		if (bChangesMade)
+		// We check the shutdown type because even though we are not an accept/cancel tool, we
+		// get a cancel shutdown via Ctrl+Z. In this case we definitely don't want to update because
+		// an update in an undo transaction results in a crash.
+		if (bChangesMade && ShutdownType != EToolShutdownType::Cancel)
 		{
 			GetToolManager()->BeginUndoTransaction(LOCTEXT("CubeGridToolEditTransactionName", "Block Tool Edit"));
 			UE::ToolTarget::CommitDynamicMeshUpdate(Target, *CurrentMesh, true);
 			GetToolManager()->EndUndoTransaction();
 		}
 	}
-	else if (CurrentMesh->TriangleCount() > 0)
+	else if (CurrentMesh->TriangleCount() > 0 && ShutdownType != EToolShutdownType::Cancel)
 	{
 		GetToolManager()->BeginUndoTransaction(LOCTEXT("CubeGridToolCreateTransactionName", "Block Tool Create New"));
 
