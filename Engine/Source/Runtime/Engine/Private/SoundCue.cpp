c6f2e73b0c3c3bd7e40a6279ec0486626628f355
Aaron McLeran <Aaron.McLeran@epicgames.com>
2022-04-27 20:10:45 -0400

    Fix for quality nodes not being respected in sound cues
    #jira UE-145226
    #rb maxwell.hayes, jimmy.smith
    #preflight 6269b9e34114e1869cca67da
    
    [CL 19950195 by Aaron McLeran in 5.0 branch]

--- Engine/Source/Runtime/Engine/Private/SoundCue.cpp
+++ Engine/Source/Runtime/Engine/Private/SoundCue.cpp
@@ -212,7 +212,7 @@ void USoundCue::PostLoad()
 #endif // WITH_EDITOR
 
 	// Warn if the Quality index is set to something that we can't support.
-	UE_CLOG(USoundCue::GetCachedQualityLevel() != CookedQualityIndex && CookedQualityIndex != INDEX_NONE, LogAudio, Warning,
+	UE_CLOG(USoundCue::GetCachedQualityLevel() != CookedQualityIndex && CookedQualityIndex != INDEX_NONE, LogAudio, Verbose,
 		TEXT("'%s' is ingoring Quality Setting '%s'(%d) as it was cooked with '%s'(%d)"),
 		*GetFullNameSafe(this),
 		*GetDefault<UAudioSettings>()->FindQualityNameByIndex(USoundCue::GetCachedQualityLevel()),
