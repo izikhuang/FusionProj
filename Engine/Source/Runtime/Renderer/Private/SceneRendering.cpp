15f7b11b954bcb3683b4112a2b8cc73b5ef0928c
jeannoe morissette <jeannoe.morissette@epicgames.com>
2022-06-14 10:03:58 -0400

    Fix uniform buffer leak with instanced stereo.
    #rb robert.srinivasiah
    #jira UE-150571
    #preflight 62a264447e3e869b97d99395
    #rnx
    #lockdown Mihnea.Balta
    
    [CL 20647711 by jeannoe morissette in 5.0 branch]

--- Engine/Source/Runtime/Renderer/Private/SceneRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/SceneRendering.cpp
@@ -2142,6 +2142,7 @@ void FViewInfo::DestroyAllSnapshots(FParallelMeshDrawCommandPass::EWaitThread Wa
 	for (FViewInfo* Snapshot : ViewInfoSnapshots)
 	{
 		Snapshot->ViewUniformBuffer.SafeRelease();
+		Snapshot->InstancedViewUniformBuffer.SafeRelease();
 		Snapshot->CachedViewUniformShaderParameters.Reset();
 
 		for (int32 Index = 0; Index < Snapshot->ParallelMeshDrawCommandPasses.Num(); ++Index)
